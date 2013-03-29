643adf0a5265ba1cdb09023b32c2e7a70ee71af7
pshegde <pshegde@ncsu.edu>
2013-03-29 16:45:25 -0400

    added launcher and added activities

--- src/com/example/iim/MainActivity.java
+++ src/com/example/iim/MainActivity.java
@@ -4,7 +4,10 @@ import com.example.iim.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +15,35 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button btnImpCallers  = (Button) findViewById(R.id.btnImpCallers);
+
+		// Listening to login button
+		btnImpCallers.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent i = new Intent(getApplicationContext(), ImpCallerActivity.class);
+				startActivity(i);
+			}
+		});
+		
+		Button btnNotType  = (Button) findViewById(R.id.btnNotType);
+
+		// Listening to login button
+		btnNotType.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent i = new Intent(getApplicationContext(), NotificationTypeActivity.class);
+				startActivity(i);
+			}
+		});
+		
+		Button btnImportCal  = (Button) findViewById(R.id.btnImportCal);
+
+		// Listening to login button
+		btnImportCal.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+			}
+		});
 	}
 
 	@Override
