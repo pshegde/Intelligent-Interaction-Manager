6de0643658001868f23263b8c575744601fa0b1b
anuprohamare <anuprohamare@192.168.154.1>
2013-03-30 21:40:19 -0400

    Did changes in create group for compatibility with Nexus S

--- src/com/iim/utils/CallerGroupManager.java
+++ src/com/iim/utils/CallerGroupManager.java
@@ -1,13 +1,18 @@
 package com.iim.utils;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.ContentProviderOperation;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.provider.ContactsContract.RawContacts;
 import android.util.Log;
 
 public class CallerGroupManager {
@@ -20,10 +25,26 @@ public class CallerGroupManager {
 	
 	public void createGroup(){
 		ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
-
+		
+		// Group already exists
+		if(getGroupIdOfImportantGroup()!=null){
+			return;
+		}
+		
+		String googleAccount = getGoogleAccountUsername();
+		
+		if(googleAccount == null){
 	    ops.add(ContentProviderOperation
 	            .newInsert(ContactsContract.Groups.CONTENT_URI)
 	            .withValue(ContactsContract.Groups.TITLE, "Important").build());
+		}
+		else{
+			 ops.add(ContentProviderOperation
+			            .newInsert(ContactsContract.Groups.CONTENT_URI)
+			            .withValue(RawContacts.ACCOUNT_TYPE, "com.google") 
+			            .withValue(RawContacts.ACCOUNT_NAME, googleAccount +"@gmail.com")
+			            .withValue(ContactsContract.Groups.TITLE, "Important").build());
+		}
 	    try {
 
 	        context.getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
@@ -37,7 +58,7 @@ public class CallerGroupManager {
 		
 		//Get group Id of the group we created - Important
 		String gidOfImportantGroup = getGroupIdOfImportantGroup();
-	    
+	    System.out.println("Group id" +  gidOfImportantGroup);
 	    Uri groupURI = ContactsContract.Data.CONTENT_URI;
 
 	    String[] projection = new String[]{
@@ -47,7 +68,9 @@ public class CallerGroupManager {
 
 	    Cursor c = this.context.getContentResolver().query(groupURI,
 	    projection,
-	    ContactsContract.CommonDataKinds.GroupMembership.GROUP_ROW_ID+"="+gidOfImportantGroup,
+	    ContactsContract.CommonDataKinds.GroupMembership.GROUP_ROW_ID+"="+gidOfImportantGroup + " AND "
+                + ContactsContract.CommonDataKinds.GroupMembership.MIMETYPE + "='"
+                + ContactsContract.CommonDataKinds.GroupMembership.CONTENT_ITEM_TYPE + "'",
 	    null,null);
 	    
 	    ArrayList<String> importantContacts = new ArrayList<String>();
@@ -59,7 +82,7 @@ public class CallerGroupManager {
 	    	importantContacts.add(s);
 	    	System.out.println(s);
 	    }
-	    
+	    System.out.println(importantContacts);
 	    return importantContacts;
 	}
 
@@ -88,6 +111,27 @@ public class CallerGroupManager {
 	    return null;
 	}
 
+	public String getGoogleAccountUsername(){
+		    AccountManager manager = AccountManager.get(this.context); 
+		    Account[] accounts = manager.getAccountsByType("com.google"); 
+		    List<String> possibleEmails = new LinkedList<String>();
+
+		    for (Account account : accounts) {
+		      // TODO: Check possibleEmail against an email regex or treat
+		      // account.name as an email address only for certain account.type values.
+		      possibleEmails.add(account.name);
+		    }
+
+		    if(!possibleEmails.isEmpty() && possibleEmails.get(0) != null){
+		        String email = possibleEmails.get(0);
+		        String[] parts = email.split("@");
+		        if(parts.length > 0 && parts[0] != null)
+		            return parts[0];
+		        else
+		            return null;
+		    }else
+		        return null;
+	}
 	
 	/*public Uri addContactToImportantGroup(long personId, long groupId){
 		//ContactsContract.
