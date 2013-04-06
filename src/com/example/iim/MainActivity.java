bef709476a9bdd915452c6293614cd200487480b
anuprohamare <anuprohamare@192.168.1.11>
2013-04-06 03:48:10 -0400

    Display alert when no email account configured.

--- src/com/example/iim/MainActivity.java
+++ src/com/example/iim/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.iim;
 
 import java.util.List;
 
+import com.iim.utils.CallerGroupManager;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -19,6 +24,21 @@ public class MainActivity extends Activity {
 
 		Button btnImpCallers  = (Button) findViewById(R.id.btnImpCallers);
 
+		CallerGroupManager callerGroupManager = new CallerGroupManager(getApplicationContext());
+		String googleAccount = callerGroupManager.getGoogleAccount();
+		if(googleAccount==null){
+			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+			alertDialog.setTitle("Google Account");
+			alertDialog.setMessage("There is no Google account configured. Please configure your Google account from Settings");
+			alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+				// add function here
+				}
+			});
+			alertDialog.show();
+		}
+		
+		
 		// Listening to login button
 		btnImpCallers.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
