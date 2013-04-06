911e8ecb64fb729a737ddb7e42a4306e5ab39287
pshegde <pshegde@ncsu.edu>
2013-04-05 20:54:07 -0400

    added listener for the button groups

--- src/com/example/iim/NotificationImpCaller.java
+++ src/com/example/iim/NotificationImpCaller.java
@@ -1,8 +1,12 @@
 package com.example.iim;
 
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class NotificationImpCaller extends Activity {
 
@@ -10,6 +14,9 @@ public class NotificationImpCaller extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_notification_imp_caller);
+		
+		// Listening to login button
+		
 	}
 
 	@Override
@@ -19,4 +26,27 @@ public class NotificationImpCaller extends Activity {
 		return true;
 	}
 
+	public void onRadioButtonClicked(View view) {
+	    // Is the button now checked?
+	    boolean checked = ((RadioButton) view).isChecked();
+	    
+	    // Check which radio button was clicked
+	    switch(view.getId()) {
+	        case R.id.rbtnAlarm:
+	            if (checked){
+	            	  Toast.makeText(NotificationImpCaller.this, "Alarm is Selected", Toast.LENGTH_SHORT).show();
+	            }
+	            break;
+	        case R.id.rbtnEmail:
+	            if (checked){
+	            	  Toast.makeText(NotificationImpCaller.this, "Email is Selected", Toast.LENGTH_SHORT).show();
+	            }
+	            break;
+	        case R.id.rbtnVibrate:
+	            if (checked){
+	            	  Toast.makeText(NotificationImpCaller.this, "Vibrate is Selected", Toast.LENGTH_SHORT).show();
+	            }
+	            break;
+	    }
+	}
 }
--- src/com/example/iim/NotificationNotImpCaller.java
+++ src/com/example/iim/NotificationNotImpCaller.java
@@ -3,6 +3,9 @@ package com.example.iim;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class NotificationNotImpCaller extends Activity {
 
@@ -17,6 +20,30 @@ public class NotificationNotImpCaller extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.notification_not_imp_caller, menu);
 		return true;
+	}
+	
+	public void onRadioButtonClicked(View view) {
+	    // Is the button now checked?
+	    boolean checked = ((RadioButton) view).isChecked();
+	    
+	    // Check which radio button was clicked
+	    switch(view.getId()) {
+	        case R.id.rbtnAlarm:
+	            if (checked){
+	            	  Toast.makeText(NotificationNotImpCaller.this, "Alarm is Selected", Toast.LENGTH_SHORT).show();
+	            }
+	            break;
+	        case R.id.rbtnEmail:
+	            if (checked){
+	            	  Toast.makeText(NotificationNotImpCaller.this, "Email is Selected", Toast.LENGTH_SHORT).show();
+	            }
+	            break;
+	        case R.id.rbtnVibrate:
+	            if (checked){
+	            	  Toast.makeText(NotificationNotImpCaller.this, "Vibrate is Selected", Toast.LENGTH_SHORT).show();
+	            }
+	            break;
+	    }
 	}
 
 }
